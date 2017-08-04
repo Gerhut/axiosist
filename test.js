da44b34c23009bba642e779e0bdc2db3495b8f71
George Chung <Gerhut@GMail.com>
2017-08-04 18:46:22 +0800

    Use ava instead of mocha

--- test.js
+++ test.js
@@ -1,87 +1,77 @@
-/* eslint-env mocha */
-'use strict'
+import test from 'ava'
+import axiosist from '.'
 
-const assert = require('assert')
-const axiosist = require('.')
+test('should request', t =>
+  axiosist((req, res) => res.end())
+    .request({ url: '/' })
+    .then(() => t.pass()))
 
-describe('axiosist', () => {
-  it('should request the callback', () => {
-    let called = false
-    return axiosist((req, res) => {
-      called = true
-      res.end()
-    }).request({ url: '/' }).then(function () {
-      assert.ok(called)
-    })
-  })
-  it('should request the right method', () => {
-    return axiosist((req, res) => {
-      assert.strictEqual('POST', req.method)
-      res.end()
-    }).post('/')
-  })
-  it('should request the right url', () => {
-    return axiosist((req, res) => {
-      assert.strictEqual('/foo/bar?baz=qux', req.url)
-      res.end()
-    }).request({
-      baseURL: '/foo',
-      url: '/bar',
-      params: { baz: 'qux' }
-    })
-  })
-  it('should request the right header', () => {
-    return axiosist((req, res) => {
-      assert.strictEqual('bar', req.headers.foo)
-      res.end()
-    }).request({
-      url: '/',
-      headers: { foo: 'bar' }
-    })
-  })
-  it('should request the right body', () => {
-    return axiosist((req, res) => {
-      req.setEncoding('utf8')
-      req.on('data', chunk => {
-        assert.deepEqual('foo', chunk)
-      }).on('end', () => res.end())
-    }).request({
-      method: 'post',
-      url: '/',
-      data: 'foo'
-    })
-  })
-  it('should request the right host', () => {
-    return axiosist((req, res) => {
-      assert.deepEqual('example.com', req.headers.host)
-      res.end()
-    }).request({
-      url: 'http://example.com/foo'
-    })
-  })
-  it('should response the right status', () => {
-    return axiosist((req, res) => {
-      res.statusCode = 400
-      res.statusMessage = 'FOO'
-      res.end()
-    }).request({ url: '/' }).catch(error => {
-      assert.strictEqual(400, error.response.status)
-      assert.strictEqual('FOO', error.response.statusText)
-    })
-  })
-  it('should response the right header', () => {
-    return axiosist((req, res) => {
-      res.setHeader('foo', 'bar')
-      res.end()
-    }).request({ url: '/' }).then(response => {
-      assert.strictEqual('bar', response.headers.foo)
-    })
-  })
-  it('should response the right body', () => {
-    return axiosist((req, res) => {
-      res.end('foo')
-    }).request({ url: '/' }).then(response => {
-      assert.strictEqual('foo', response.data)
-    })
-  })
-})
+test('should request the right method', t =>
+  axiosist((req, res) => {
+    t.is(req.method, 'POST')
+    res.end()
+  }).post('/'))
+
+test('should request the right url', t =>
+  axiosist((req, res) => {
+    t.is(req.url, '/foo/bar?baz=qux')
+    res.end()
+  }).request({
+    baseURL: '/foo',
+    url: '/bar',
+    params: { baz: 'qux' }
+  }))
+
+test('should request the right header', t =>
+  axiosist((req, res) => {
+    t.is(req.headers.foo, 'bar')
+    res.end()
+  }).request({
+    url: '/',
+    headers: { foo: 'bar' }
+  }))
+
+test('should request the right body', t =>
+  axiosist((req, res) => {
+    req.setEncoding('utf8')
+    req
+      .on('data', chunk => t.is(chunk, 'foo'))
+      .on('end', () => res.end())
+  }).request({
+    method: 'post',
+    url: '/',
+    data: 'foo'
+  }))
+
+test('should request the right host', t =>
+  axiosist((req, res) => {
+    t.is(req.headers.host, 'example.com')
+    res.end()
+  }).request({
+    url: 'http://example.com/foo'
+  }))
+
+test('should response the right status', t =>
+  axiosist((req, res) => {
+    res.statusCode = 400
+    res.statusMessage = 'FOO'
+    res.end()
+  }).request({ url: '/' }).catch(error => {
+    t.is(error.response.status, 400)
+    t.is(error.response.statusText, 'FOO')
+  }))
+
+test('should response the right header', t =>
+  axiosist((req, res) => {
+    res.setHeader('foo', 'bar')
+    res.end()
+  }).request({ url: '/' }).then(response => {
+    t.is(response.headers.foo, 'bar')
+  }))
+
+test('should response the right body', t =>
+  axiosist((req, res) => {
+    res.end('foo')
+  }).request({ url: '/' }).then(response => {
+    t.is(response.data, 'foo')
+  }))
