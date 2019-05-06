d6ce6ce8177dd9de42ca74081a9191c243f69104
George Cheng <Gerhut@GMail.com>
2019-05-06 12:37:59 +0800

    Remove unnesserary promise chain for non-listening

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const createAdapter = handler => config => {
   const server = handler instanceof http.Server ? handler : http.createServer(handler)
   const listening = server.listening
 
-  return new Promise((resolve, reject) => {
+  let promise = new Promise((resolve, reject) => {
     server.on('error', reject)
     if (listening) {
       resolve()
@@ -64,19 +64,16 @@ const createAdapter = handler => config => {
     urlObject.port = address.port.toString()
     config.url = url.format(urlObject)
     return defaultAdapter(config)
-  }).then(value => new Promise(resolve => {
-    if (listening) {
-      resolve(value)
-    } else {
-      server.close(() => resolve(value))
-    }
-  }), reason => new Promise((resolve, reject) => {
-    if (listening) {
-      reject(reason)
-    } else {
-      server.close(() => reject(reason))
-    }
-  }))
+  })
+
+  if (!listening) {
+    promise = promise.then(
+      value => new Promise(resolve => server.close(() => resolve(value))),
+      reason => new Promise((resolve, reject) => server.close(() => reject(reason)))
+    )
+  }
+
+  return promise
 }
 
 /**
