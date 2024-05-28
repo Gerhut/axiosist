f418cc946c7297341b4baa83da06cf6770656860
pagewang0 <pagewang0@gmail.com>
2024-05-28 19:39:58 +0800

    merge upstream repo

--- index.js
+++ index.js
@@ -51,9 +51,7 @@ const createAdapter = handler => config => new Promise((resolve, reject) => {
   const server = handler instanceof Server ? handler : createServer(handler)
   const listening = server.listening
 
-  if (server.eventNames().includes('error')) {
-    server.on('error', reject)
-  }
+  server.on('error', reject);
 
   /** @type {Promise<void>} */ (
     new Promise(resolve => {
